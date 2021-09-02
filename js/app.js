0310f83697477fbdb2f5387b020ba50da25163e7
iqbalcb88 <iqbal.cb88@gmail.com>
2021-09-02 14:22:24 +0600

    added default cover photo

--- js/app.js
+++ js/app.js
@@ -5,22 +5,22 @@ const loadData = () => {
   const inputText = inputField.value;
 
   // console.log(inputValue);
-  const url = `http://openlibrary.org/search.json?q=${inputText}`;
+  const url = `https://openlibrary.org/search.json?q=${inputText}`;
   fetch(url)
     .then((res) => res.json())
     .then((data) => displayData(data));
+  inputField.value = '';
 };
 
 const booksNumber = document.getElementById('books-number');
 const cardDiv = document.getElementById('cards');
 
 const displayData = (booksObject) => {
+  cardDiv.textContent = '';
   // console.log(booksObject);
-  console.log('clicked');
+  // console.log('clicked');
   const numOfBooks = booksObject.numFound;
-  if (inputField.value.length === 0) {
-    booksNumber.innerText = 'Please input your Books Name';
-  } else if (numOfBooks === 0) {
+  if (numOfBooks === 0) {
     booksNumber.innerText = `Sorry!!! No Books Found.`;
   } else {
     booksNumber.innerText = `About ${booksObject.numFound} Books Found.`;
@@ -33,15 +33,20 @@ const displayData = (booksObject) => {
     // console.log(element);
     if (index <= 4) {
       console.log(element);
-      const imgUrl = `https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg`;
-      console.log(imgUrl);
+      let imgUrl = `https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg`;
+      // if cover not found default cover photo will set
+      if (element.cover_i === undefined) {
+        imgUrl = `img/default.jpg`;
+      }
+
+      // console.log(imgUrl);
       const singleCard = document.createElement('div');
       singleCard.classList.add('card', 'col-4');
       const authorsArr = element.author_name;
       singleCard.innerHTML = `
-      <img src="${imgUrl}" class="card-img-top" alt="Book Cover" />
+      <img src="${imgUrl}" height=300 class="card-img-top" alt="Book Cover Not Found" />
         <div class="card-body">
-          <h5 class="card-title">Books Name: ${element.title}</h5>
+          <h5 class="card-title text-white bg-success">Books Name: ${element.title}</h5>
           <h6v id="author" class="card-title">First Publish Year: ${element.first_publish_year}</h6v>
           <p class="card-text">Authors: ${authorsArr}</p>
         </div>
